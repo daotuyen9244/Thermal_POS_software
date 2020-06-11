b785f11a87629338082bcabac2447bd3d52e7dca
daotuyen <daotuyen9244@gmail.com>
2020-06-11 22:22:34 +0700

    fix source

--- flash_usb_printer/flash_usb_printer.ino
+++ flash_usb_printer/flash_usb_printer.ino
@@ -6,29 +6,16 @@ void setup()
   /* Set baud rate for serial communication */
   Serial.begin(115200);
   delay(100);
-  #ifdef USE_FLASH
+#ifdef USE_FLASH
   initFlash();
-  //WriteDataToFlash();
-  //ReadDataFromFlash();
-  #endif
-  #ifdef USE_ETHERNET
+//WriteDataToFlash();
+//ReadDataFromFlash();
+#endif
+#ifdef USE_ETHERNET
   initEthernet();
-  #endif
-  tpSetBackBuffer(sourceBuf, WIDTH, HEIGHT);
+#endif
+  tpSetBackBuffer(sourcePrint, WIDTH, HEIGHT);
   tpFill(0);
-  ImgLoadStatus = tpLoadBMP((uint8_t *)logo_bmp, 1, 0, 0);
-  if (ImgLoadStatus != 0)
-  {
-    Serial.println("tpLoadBMP error!");
-    /*while (1)
-      {
-      };*/
-  }
-  else
-  {
-    Serial.println("tpLoadBMP OK!");
-  }
-
 }
 
 void loop()
@@ -36,21 +23,49 @@ void loop()
   // put your main code here, to run repeatedly:
   byte inChar;
   inChar = Serial.read();
-  #ifdef USE_ETHERNET
-  if(inChar == 'r')
+#ifdef USE_ETHERNET
+  if (inChar == 'r')
   {
-    if(doFTP(fileName)) Serial.println(F("FTP OK"));
-    else Serial.println(F("FTP FAIL"));
+    Serial.println(">>>>>>>>>>>>>READ FTP >>>>>>>>>>>>");
+    if (doFTP(fileName))
+      Serial.println(F("FTP OK"));
+    else
+      Serial.println(F("FTP FAIL"));
   }
-  #endif
-#ifdef USE_PRINTER
-if(inChar == 'p')
+#endif
+  if (inChar == 'f')
   {
-    downloadStatus =true;
-  }
-  if(downloadStatus)
-  {
+    Serial.println(">>>>>>>>>>>>>READ FLS >>>>>>>>>>>>");
     ReadDataFromFlash(24);
+  }
+#ifdef IMAGE_PROCESS
+  if (inChar == 'c')
+  {
+    Serial.println(">>>>>>>>>>>>>Convert data >>>>>>>>>>>>");
+    converTo1BBp();
+    ImgLoadStatus = tpLoadBMP((uint8_t *)sourceBuf, 1, 0, 0);
+    if (ImgLoadStatus != 0)
+    {
+      Serial.println("tpLoadBMP error!");
+      /*while (1)
+      {
+      };*/
+    }
+    else
+    {
+      Serial.println("tpLoadBMP OK!");
+    }
+  }
+
+#endif
+#ifdef USE_PRINTER
+  if (inChar == 'p')
+  {
+    downloadStatus = true;
+  }
+  if (downloadStatus)
+  {
+    //ReadDataFromFlash(24);
   }
   printPicture();
 #endif
