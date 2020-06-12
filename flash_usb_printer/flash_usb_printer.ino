92719a295e35c4e10f4f88448bb0835aed6445e3
TuyenDao <daotuyen9244@gmail.com>
2020-06-12 11:44:16 +0700

    load and convert data ok

--- flash_usb_printer/flash_usb_printer.ino
+++ flash_usb_printer/flash_usb_printer.ino
@@ -9,13 +9,14 @@ void setup()
 #ifdef USE_FLASH
   initFlash();
 //WriteDataToFlash();
-//ReadDataFromFlash();
+//ReadDataFromFlash(1);
 #endif
 #ifdef USE_ETHERNET
   initEthernet();
 #endif
-  tpSetBackBuffer(sourcePrint, WIDTH, HEIGHT);
-  tpFill(0);
+  //tpSetBackBuffer(sourcePrint, WIDTH, HEIGHT);
+  //tpFill(0);
+  getFreeRAM();
 }
 
 void loop()
@@ -23,38 +24,72 @@ void loop()
   // put your main code here, to run repeatedly:
   byte inChar;
   inChar = Serial.read();
+
 #ifdef USE_ETHERNET
   if (inChar == 'r')
   {
-    Serial.println(">>>>>>>>>>>>>READ FTP >>>>>>>>>>>>");
+    Serial.println(F(">>>>>>>>>>>>>READ FTP >>>>>>>>>>>>"));
     if (doFTP(fileName))
       Serial.println(F("FTP OK"));
     else
       Serial.println(F("FTP FAIL"));
+      getFreeRAM();
   }
 #endif
   if (inChar == 'f')
   {
-    Serial.println(">>>>>>>>>>>>>READ FLS >>>>>>>>>>>>");
+    Serial.println(F(">>>>>>>>>>>>>READ FLS >>>>>>>>>>>>"));
     ReadDataFromFlash(24);
+    getFreeRAM();
   }
-#ifdef IMAGE_PROCESS
-  if (inChar == 'c')
+  if (inChar == 's')
   {
-    Serial.println(">>>>>>>>>>>>>Convert data >>>>>>>>>>>>");
-    converTo1BBp();
+    getFreeRAM();
+    WriteDataToFlash();
+ReadDataFromFlash(24);
+  getFreeRAM();
+  }
+  if (inChar == 'l')
+  {
+    ReadDataFromFlash(1);
+  }
+  if (inChar == 'd')
+  {
     ImgLoadStatus = tpLoadBMP((uint8_t *)sourceBuf, 1, 0, 0);
     if (ImgLoadStatus != 0)
     {
-      Serial.println("tpLoadBMP error!");
+      Serial.println(F("tpLoadBMP error!"));
       /*while (1)
       {
       };*/
     }
     else
     {
-      Serial.println("tpLoadBMP OK!");
+      Serial.println(F("tpLoadBMP OK!"));
     }
+  }
+#ifdef IMAGE_PROCESS
+  if (inChar == 'c')
+  {
+    Serial.println(F(">>>>>>>>>>>>>Convert data >>>>>>>>>>>>"));
+    getFreeRAM();
+    converTo1BBp();
+    getFreeRAM();
+    tpSetBackBuffer(sourcePrint, WIDTH, HEIGHT);
+    tpFill(0);
+    ImgLoadStatus = tpLoadBMP((uint8_t *)sourceBuf, 1, 0, 0);
+    if (ImgLoadStatus != 0)
+    {
+      Serial.println(F("tpLoadBMP error!"));
+      /*while (1)
+      {
+      };*/
+    }
+    else
+    {
+      Serial.println(F("tpLoadBMP OK!"));
+    }
+    getFreeRAM();
   }
 
 #endif
